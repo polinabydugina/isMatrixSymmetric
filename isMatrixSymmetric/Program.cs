35ed01fec48ec00ee49319989897afae31bd8353
212224 <xch.victor@mail.ru>
2023-11-16 13:01:13 +0300

    Готово

--- isMatrixSymmetric/Program.cs
+++ isMatrixSymmetric/Program.cs
@@ -1,47 +1,53 @@
-﻿        int x = Convert.ToInt32(Console.ReadLine());
+﻿
+        int x = Convert.ToInt32(Console.ReadLine());
         int[,] matrix = new int[x, x];
-
-        for (int i = 0; i < x; i++)
+if (x <= 100)
+{
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < x; j++)
         {
-            for (int j = 0; j < x; j++)
-            {
-              matrix[i, j] = Convert.ToInt32(Console.ReadLine());
-            }
+            matrix[i, j] = Convert.ToInt32(Console.ReadLine());
         }
+    }
 
-    
 
-        static bool isMatrixSymmetric(int[,] matrix)
-        {
+    static bool isMatrixSymmetric(int[,] matrix)
+    {
         int n = matrix.GetLength(0);
         for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
             {
-                for (int j = i + 1; j < n; j++)
+                if (matrix[i, j] != matrix[j, i])
                 {
-                    if (matrix[i, j] != matrix[j, i])
-                    {
                     return false;
-                    }
                 }
-
             }
-            return true;
-        }
-        
-        for(int i=0; i<x; i++)
-{
-        for(int j = 0; j<x; j++)
-    {
-        Console.Write($"{matrix[i, j]}");
-    }
-    Console.WriteLine();
-}
 
-if (isMatrixSymmetric(matrix))
-{
-    Console.WriteLine("YES");
-}
+        }
+        return true;
+    }
+
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
+            Console.Write($" {matrix[i, j]}");
+        }
+        Console.WriteLine();
+    }
+
+    if (isMatrixSymmetric(matrix))
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+    }
 else
 {
-    Console.WriteLine("NO");
+    Console.WriteLine("х должен быть меньше 100");
 }
