fb7836106e4d2d6e0f58eae97793480fe0c042a6
polinabydugina <xch.victor@mail.ru>
2023-11-26 20:34:02 +0300

    во теперь всё

--- isMatrixSymmetric/Program.cs
+++ isMatrixSymmetric/Program.cs
@@ -1,53 +1,56 @@
-﻿
-        int x = Convert.ToInt32(Console.ReadLine());
-        int[,] matrix = new int[x, x];
-if (x <= 100)
+﻿while (true)
 {
-    for (int i = 0; i < x; i++)
+    int x = Convert.ToInt32(Console.ReadLine());
+    int[,] matrix = new int[x, x];
+    if (x <= 100)
     {
-        for (int j = 0; j < x; j++)
+        for (int i = 0; i < x; i++)
         {
-            matrix[i, j] = Convert.ToInt32(Console.ReadLine());
-        }
-    }
-
-
-    bool isMatrixSymmetric(int[,] matrix)
-    {
-        int n = matrix.GetLength(0);
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = i + 1; j < n; j++)
+            for (int j = 0; j < x; j++)
             {
-                if (matrix[i, j] != matrix[j, i])
-                {
-                    return false;
-                }
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
             }
-
         }
-        return true;
-    }
-    Console.Clear();
-    for (int i = 0; i < x; i++)
-    {
-        for (int j = 0; j < x; j++)
+
+
+        bool isMatrixSymmetric(int[,] matrix)
         {
-            Console.Write($" {matrix[i, j]}");
-        }
-        Console.WriteLine();
-    }
+            int n = matrix.GetLength(0);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (matrix[i, j] != matrix[j, i])
+                    {
+                        return false;
+                    }
+                }
 
-    if (isMatrixSymmetric(matrix))
-    {
-        Console.WriteLine("YES");
+            }
+            return true;
+        }
+        Console.Clear();
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                Console.Write($" {matrix[i, j]}");
+            }
+            Console.WriteLine();
+        }
+
+        if (isMatrixSymmetric(matrix))
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+        break;
     }
     else
     {
-        Console.WriteLine("NO");
+        Console.WriteLine("х должен быть меньше 100");
     }
-    }
-else
-{
-    Console.WriteLine("х должен быть меньше 100");
 }
